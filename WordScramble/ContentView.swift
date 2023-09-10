99a2b06b2421b3a4e4a59cf52e2ce0ac4094cf41
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-10 16:21:22 +0530

    App complete

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,33 +8,94 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTtile = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+
     var body: some View {
-        List(0..<5) {
-            Text("Dynamic row \($0)")
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTtile, isPresented: $showingError) {
+                Button("Ok", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
         }
-        .listStyle(.sidebar)
     }
     
-    func test() {
-        let input = """
-a
-b
-c
-"""
-        let letters = input.components(separatedBy: "\n")
-        let letter = letters.randomElement()
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
         
-        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard isOriginal(word: answer) else { return wordError(title: "Word used alredy", message: "Be more original") }
+        guard isPossible(word: answer) else { return wordError(title: "Word not possible", message: "You can't spell that from '\(rootWord)'") }
+        guard isReal(word: answer) else { return wordError(title: "Word not recognised", message: "You can't just make them up, you know!") }
         
-//        UITextChecker - check spelling errors
-        
-        let word = "swift"
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        fatalError("Could not start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
         let checker = UITextChecker()
-        
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        
-        let allGood = misspelledRange.location == NSNotFound
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTtile = title
+        errorMessage = message
+        showingError = true
     }
 }
 
