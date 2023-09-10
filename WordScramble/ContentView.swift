d4550c57d5fb73370daeeb4593307eadd971b894
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-10 15:27:28 +0530

    String APIs, spell check using UITextChecker

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -15,12 +15,26 @@ struct ContentView: View {
         .listStyle(.sidebar)
     }
     
-    func loadFile() {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                
-            }
-        }
+    func test() {
+        let input = """
+a
+b
+c
+"""
+        let letters = input.components(separatedBy: "\n")
+        let letter = letters.randomElement()
+        
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+//        UITextChecker - check spelling errors
+        
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
     }
 }
 
