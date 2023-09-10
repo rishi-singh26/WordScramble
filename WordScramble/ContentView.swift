34778b56c151e4462889907a48ca92f179fad337
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-10 15:04:20 +0530

    List without ForEach

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -9,20 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List {
-            Section("Secton 1") {
-                Text("Statci row 1")
-                Text("Statci row 2")
-            }
-            Section("Secton 2") {
-                ForEach(0..<5) {
-                    Text("Dynamic row \($0)")
-                }
-            }
-            Section("Secton 3") {
-                Text("Statci row 3")
-                Text("Statci row 4")
-            }
+        List(0..<5) {
+            Text("Dynamic row \($0)")
         }
         .listStyle(.sidebar)
     }
